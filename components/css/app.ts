71f28ae4fa1a8cf6b0c5afb24b691e36f4ef97a3
Michael-enin <sys.dev.mike@gmail.com>
2025-05-04 09:05:21 +0300

    styling

--- components/css/app.ts
+++ components/css/app.ts
@@ -248,17 +248,17 @@ const getStyles = (isDarkMode:boolean) =>{
       justifyContent: 'flex-start', // Align at top
       alignItems: 'flex-end', // Align to right
       margin: 0,
-      paddingTop: height * 0.075, // Adjust depending on navbar height
+      paddingTop: height * 0.08, // Adjust depending on navbar height
       paddingRight: width * 0.03,
     },
     modalContent: {     
       borderRadius: 8,
       width: width * 0.3,
       // marginTop: height * -0.315,
-       marginLeft: width * 0.645,
-       marginRight: width * 0.0001,
+      marginLeft: width * 0.645,
+      marginRight: width * 0.0001,
       paddingRight: width * 0.06, 
-       borderWidth:isDarkMode?0:2,
+      borderWidth:isDarkMode?0:2,
       borderColor:'white',
       backgroundColor: isDarkMode?'#fff': "black",
 
